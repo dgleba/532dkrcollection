b12e8809f5355283d1ad170648f124067335e6c3
David Gleba <dgleba@gmail.com>
2020-12-13 20:08:50 -0500

    2020-12-13.change.login.add.djangorules.example

---
+++ 533-djangorules-yard/533-dkrs-djangoexample/mangosite/blogapp/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+
+from . import models
+
+
+class PostSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = models.Post
+        fields = [
+            "created",
+            "last_updated",
+            "title",
+            "body",
+        ]
